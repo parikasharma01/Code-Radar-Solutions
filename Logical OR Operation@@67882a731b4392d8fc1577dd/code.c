b52c3264abcdba9cd9326dea7697f69651b9b7c1
parikasharma01 <parika1474.becse24@chitkara.edu.in>
2025-01-28 10:58:45 +0530

    Solution to Logical OR Operation

---
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if((a>o)||(b>0)){
+        pritnf("True");
+    }
+    else{
+        pritnf("False");
+    }
+    return 0;
+}
