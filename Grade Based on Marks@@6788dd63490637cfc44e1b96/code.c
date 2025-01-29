cd077f72c0077b07bb68e3d79ee7cba9b454c1fe
parikasharma01 <parika1474.becse24@chitkara.edu.in>
2025-01-29 17:13:42 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -15,10 +15,10 @@ int main(){
         printf("D");
     }
     else if(a < 60){
-        printf("F")
+        printf("F");
     }
     else{
-        printf("Invalid")
+        printf("Invalid");
     }
     }
     
