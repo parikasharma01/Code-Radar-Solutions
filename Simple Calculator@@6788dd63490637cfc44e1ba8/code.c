fb24b392cbd88ccdcc977e1d4b70da2d2d1a87fd
parikasharma01 <parika1474.becse24@chitkara.edu.in>
2025-02-04 20:36:10 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -2,16 +2,16 @@
 int main(){
     int a,b,c ;
     scanf("%d %d %d",&a,&b,&c);
-    if(c == addition ){
+    if(c == '+' ){
         printf("%d",a+b);
     }
-    else if(c == subtraction){
+    else if(c == '-'){
         printf("%d",a-b);
     }
-    else if(c == multiplication){
+    else if(c == '*'){
         printf("%d",a*b);
     }
-    else if(c == division){
+    else if(c == '/'){
         printf("%d",a/b);
     }
     else{
